c01d4bf4dfcd2136fc89dc3508a94ccfbd71aaf7
Anant Narayan <anantnrg@gmail.com>
2023-10-02 20:43:00 +0530

    don't think gtk is the way to go

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-	println!("Hello, world!");
+mod libs;
+
+use anyhow::Result;
+use gtk::{
+	glib,
+	prelude::*,
+	Application,
+};
+use libs::gui::window::create_window;
+
+const APP_ID: &str = "scrkey";
+
+fn main() -> Result<()> {
+	let app = Application::builder().application_id(APP_ID).build();
+
+	app.connect_activate(create_window);
+
+	app.run();
+
+	Ok(())
 }
