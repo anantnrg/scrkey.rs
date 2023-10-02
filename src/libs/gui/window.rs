d938a69f65f1cb8f59be8f480bf8b79f334aa33b
Anant Narayan <anantnrg@gmail.com>
2023-10-02 21:12:36 +0530

    nuke

--- src/libs/gui/window.rs
+++ src/libs/gui/window.rs
@@ -1,18 +1,2 @@
-use gtk::{
-	glib,
-	prelude::*,
-	Application,
-	ApplicationWindow,
-};
 
-pub fn create_window(app: &Application) {
-	let window = ApplicationWindow::builder()
-		.application(app)
-		.title("ScrKey.rs")
-		.default_width(200)
-		.default_height(100)
-		.modal(true)
-		.build();
 
-	window.present();
-}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
 mod libs;
 
 use anyhow::Result;
-use gtk::{
-	glib,
-	prelude::*,
-	Application,
-};
 use libs::gui::window::create_window;
 
 const APP_ID: &str = "scrkey";
 
 fn main() -> Result<()> {
-	let app = Application::builder().application_id(APP_ID).build();
-
-	app.connect_activate(create_window);
-
-	app.run();
-
 	Ok(())
 }
