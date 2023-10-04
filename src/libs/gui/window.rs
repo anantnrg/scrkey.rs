eebd06b63a7c0d9bc765ea124d7fd725c9896c86
Anant Narayan <anantnrg@gmail.com>
2023-10-04 17:25:23 +0530

    :sparkles: Made window not movable. Lets go bois!

--- src/libs/gui/window.rs
+++ src/libs/gui/window.rs
@@ -1,10 +1,22 @@
 use anyhow::Result;
+use gdk::prelude::*;
 use gio::prelude::*;
 use gtk::{
 	prelude::*,
 	Application,
 	ApplicationWindow,
-	Window,
+};
+use x11rb::{
+	self,
+	connection::Connection,
+	protocol::xproto::{
+		ConnectionExt,
+		*,
+	},
+	rust_connection::{
+		DefaultStream,
+		RustConnection,
+	},
 };
 
 pub struct Scrkey {
@@ -27,16 +39,16 @@ impl Scrkey {
 	}
 
 	pub fn render(app: &Application) {
-		let window = ApplicationWindow::builder()
-			.application(app)
-			.title("ScrKey.rs")
-			.width_request(400)
-			.height_request(60)
-			.build();
+		let window = gtk::Window::new(gtk::WindowType::Popup);
 
 		window.set_keep_above(true);
 		window.set_decorated(false);
 		window.set_resizable(false);
+		window.move_(50, 50);
+		window.set_width_request(400);
+		window.set_height_request(60);
+		window.set_application(Some(app));
+		window.set_title("ScrKey.rs");
 		window.stick();
 
 		window.present();
